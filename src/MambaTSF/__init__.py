eaf251f2500a87b81851700ddfe637f22191f7af
Brad Nolan <brad@throttle.co.nz>
2024-08-11 08:54:22 +1200

    Bump version to 0.1.2

--- src/MambaTSF/__init__.py
+++ src/MambaTSF/__init__.py
@@ -3,4 +3,9 @@
 # This is the version number of your code.  This will change each time you make
 # a new release.  The most popular approach is to use Semantic Versioning,
 # described at https://semver.org/
-__version__ = "0.1.1"
+__version__ = "0.1.2"
+
+
+# Version History
+# 0.1.1 Intial relase with tiny and min mamba models
+# 0.1.2 Add iTransformer and utils

fd128e3fbfe09804a0aea6c23be6c620d8ceabe7
Brad Nolan <brad@throttle.co.nz>
2024-08-11 11:33:46 +1200

    Impoer modules and bump version to 0.1.3

--- src/MambaTSF/__init__.py
+++ src/MambaTSF/__init__.py
@@ -3,9 +3,27 @@
 # This is the version number of your code.  This will change each time you make
 # a new release.  The most popular approach is to use Semantic Versioning,
 # described at https://semver.org/
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 # Version History
 # 0.1.1 Intial relase with tiny and min mamba models
 # 0.1.2 Add iTransformer and utils
+# 0.1.3 Module name changes and correct imports
+
+#__all__ = [ "utils", "iTransformer", "mamba_min", "mamba_tiny", "sdMamba" ]
+
+from mamba_min import MambaMinConfig
+from mamba_min import MambaBlock as MambaBlockMin
+from mamba_min import Mamba as MambaMin
+
+from mamba_tiny import MambaTinyConfig
+from mamba_tiny import MambaBlock as MambaBlockTiny
+from mamba_tiny import Mamba as MambaTiny
+
+from mamba_sd import S_D_MambaConfig
+from mamba_sd import SDMamba
+
+from iTransformer import iTransModel
+
+import utils

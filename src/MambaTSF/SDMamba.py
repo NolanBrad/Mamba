8c7934ab289f296ef84a96106746aea5a5996a8a
Brad Nolan <brad@throttle.co.nz>
2024-08-10 18:03:02 +1200

    Add S-D-Mamba

---
+++ src/MambaTSF/SDMamba.py
@@ -0,0 +1,151 @@
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+@dataclass
+class S_D_MambaConfig:
+    seq_len: int
+    pred_len: int
+    use_norm: bool
+    e_layers: int
+    d_model: int
+    d_ff: int | None = None
+    d_state: int = 16
+    dropout: int = 0.1
+    output_attention: bool = False
+
+
+class DataEmbedding_inverted(nn.Module):
+
+    def __init__(self, c_in, d_model, dropout=0.1):
+        super(DataEmbedding_inverted, self).__init__()
+        self.value_embedding = nn.Linear(c_in, d_model)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x, x_mark):
+        x = x.permute(0, 2, 1)
+
+        # x: [Batch Variate Time]
+        if x_mark is None:
+
+            x = self.value_embedding(x)
+        else:
+            x = self.value_embedding(torch.cat([x, x_mark.permute(0, 2, 1)], 1))
+        return self.dropout(x)
+
+class Encoder(nn.Module):
+
+    def __init__(self, attn_layers, conv_layers=None, norm_layer=None):
+        super(Encoder, self).__init__()
+        self.attn_layers = nn.ModuleList(attn_layers)
+        self.conv_layers = nn.ModuleList(conv_layers) if conv_layers is not None else None
+        self.norm = norm_layer
+
+    def forward(self, x):
+        # x [B, L, D]
+
+        if self.conv_layers is not None:
+            for i, (attn_layer, conv_layer) in enumerate(zip(self.attn_layers, self.conv_layers)):
+                x, attn = attn_layer(x)
+                x = conv_layer(x)
+
+            x, _ = self.attn_layers[-1](x)
+        else:
+            for attn_layer in self.attn_layers:
+                x, _ = attn_layer(x)
+
+        if self.norm is not None:
+            x = self.norm(x)
+
+        return x, None
+
+class EncoderLayer(nn.Module):
+
+    def __init__(self, attention, attention_r, d_model, d_ff=None, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+        d_ff = d_ff or 4 * d_model
+        self.attention = attention
+        self.attention_r = attention_r
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.activation = F.relu
+
+    def forward(self, x):
+        new_x = self.attention(x) + self.attention_r(x.flip(dims=[1])).flip(dims=[1])
+
+        x = x + new_x
+        y = x = self.norm1(x)
+        y = self.dropout(self.activation(self.conv1(y.transpose(-1, 1))))
+        y = self.dropout(self.conv2(y).transpose(-1, 1))
+
+        return self.norm2(x + y), None
+
+class SDMamba(nn.Module):
+    """
+    Paper link: https://arxiv.org/abs/2310.06625
+    """
+
+    def __init__(self, configs: S_D_MambaConfig, mamba_forward, mamba_reverse):
+
+        super(SDModel, self).__init__()
+
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        self.output_attention = configs.output_attention
+        self.use_norm = configs.use_norm
+        # Embedding
+        self.enc_embedding = DataEmbedding_inverted(configs.seq_len, configs.d_model, configs.dropout)
+
+        # Encoder-only architecture
+        self.encoder = Encoder(
+            [
+                EncoderLayer(mamba_forward, mamba_reverse,
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout )
+                for l in range(configs.e_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        )
+        self.projector = nn.Linear(configs.d_model, configs.pred_len, bias=True)
+
+    def forecast(self, x_enc):
+        if self.use_norm:
+            # Normalization from Non-stationary Transformer
+            means = x_enc.mean(1, keepdim=True).detach()
+            x_enc = x_enc - means
+            stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+            x_enc /= stdev
+
+        _, _, N = x_enc.shape # B L N
+        # B: batch_size;    E: d_model;
+        # L: seq_len;       S: pred_len;
+        # N: number of variate (tokens), can also includes covariates
+
+        # Embedding
+        # B L N -> B N E                (B L N -> B L E in the vanilla Transformer)
+        enc_out = self.enc_embedding(x_enc) # covariates (e.g timestamp) can be also embedded as tokens
+
+        # B N E -> B N E                (B L E -> B L E in the vanilla Transformer)
+        # the dimensions of embedded time series has been inverted, and then processed by native attn, layernorm and ffn modules
+        enc_out, attns = self.encoder(enc_out, attn_mask=None)
+        # B N E -> B N S -> B S N
+        dec_out = self.projector(enc_out).permute(0, 2, 1)[:, :, :N] # filter the covariates
+
+        if self.use_norm:
+            # De-Normalization from Non-stationary Transformer
+            dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+            dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+
+        return dec_out
+
+
+    def forward(self, x_enc, x_dec ):
+        dec_out = self.forecast(x_enc, x_dec)
+        return dec_out[:, -self.pred_len:, :]  # [B, L, D]

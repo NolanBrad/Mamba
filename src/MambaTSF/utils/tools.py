fde363d21dd909eae9552d537d8c85bf81d79791
Brad Nolan <34460782+NolanBrad@users.noreply.github.com>
2024-08-11 19:09:30 +1200

    Update tools.py. Remove args from adjust_learning_rate

--- src/MambaTSF/utils/tools.py
+++ src/MambaTSF/utils/tools.py
@@ -7,12 +7,11 @@ import pandas as pd
 
 plt.switch_backend('agg')
 
-
-def adjust_learning_rate(optimizer, epoch, args):
+def adjust_learning_rate(optimizer, epoch, lradj = 'type1', learning_rate = 0.0001):
     # lr = args.learning_rate * (0.2 ** (epoch // 2))
-    if args.lradj == 'type1':
-        lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
-    elif args.lradj == 'type2':
+    if .lradj == 'type1':
+        lr_adjust = {epoch: learning_rate * (0.5 ** ((epoch - 1) // 1))}
+    elif lradj == 'type2':
         lr_adjust = {
             2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
             10: 5e-7, 15: 1e-7, 20: 5e-8

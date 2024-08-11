d8236faf55bc90d961c475cae5aa367f3b056dd1
Brad Nolan <brad@throttle.co.nz>
2024-08-11 12:08:46 +1200

    Correct imports try two...

--- src/MambaTSF/__init__.py
+++ src/MambaTSF/__init__.py
@@ -11,19 +11,20 @@ __version__ = "0.1.3"
 # 0.1.2 Add iTransformer and utils
 # 0.1.3 Module name changes and correct imports
 
-#__all__ = [ "utils", "iTransformer", "mamba_min", "mamba_tiny", "sdMamba" ]
+import MambaTSF.utils
 
-from mamba_min import MambaMinConfig
-from mamba_min import MambaBlock as MambaBlockMin
-from mamba_min import Mamba as MambaMin
+from MambaTSF.mamba_tiny import MambaTinyConfig
+from MambaTSF.mamba_tiny import MambaBlock as MambaBlockTiny
+from MambaTSF.mamba_tiny import Mamba as MambaTiny
 
-from mamba_tiny import MambaTinyConfig
-from mamba_tiny import MambaBlock as MambaBlockTiny
-from mamba_tiny import Mamba as MambaTiny
+from MambaTSF.mamba_min import MambaMinConfig
+from MambaTSF.mamba_min import MambaBlock as MambaBlockMin
+from MambaTSF.mamba_min import Mamba as MambaMin
 
-from mamba_sd import S_D_MambaConfig
-from mamba_sd import SDMamba
+from MambaTSF.mamba_sd import S_D_MambaConfig
+from MambaTSF.mamba_sd import SDMamba
 
-from iTransformer import iTransModel
+from MambaTSF.iTransformer import iTransModel
 
-import utils
+
+#__all__ = [ "utils", "iTransformer", "mamba_min", "mamba_tiny", "mamba_sd" ]

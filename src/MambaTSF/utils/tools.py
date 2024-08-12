82b438d47cea8865b3458c20efcbf21d9980010e
Brad Nolan <34460782+NolanBrad@users.noreply.github.com>
2024-08-12 12:48:46 +1200

    Update tools.py. adjust_learning_rate type changed to enum

--- src/MambaTSF/utils/tools.py
+++ src/MambaTSF/utils/tools.py
@@ -4,18 +4,28 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import pandas as pd
+from enum import Enum
 
 plt.switch_backend('agg')
 
-def adjust_learning_rate(optimizer, epoch, lradj = 'type1', learning_rate = 0.0001):
+class LearningRateType(Enum)
+    TYPE_1 = 0
+    TYPE_2 = 1
+    NONE = 2
+
+def adjust_learning_rate(optimizer, epoch, lradj:LearningRateType=LearningRateType.TYPE_1, learning_rate=0.0001):
     # lr = args.learning_rate * (0.2 ** (epoch // 2))
-    if .lradj == 'type1':
+    if lradj == LearningRateType=LearningRateType.TYPE_1:
         lr_adjust = {epoch: learning_rate * (0.5 ** ((epoch - 1) // 1))}
-    elif lradj == 'type2':
+    elif lradj == LearningRateType=LearningRateType.TYPE_2':
         lr_adjust = {
             2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
-            10: 5e-7, 15: 1e-7, 20: 5e-8
+            10: 5e-7, 15: 1e-7, 20: 5e-8            
         }
+    else
+    {
+        lr_adjust = {}
+    }
     if epoch in lr_adjust.keys():
         lr = lr_adjust[epoch]
         for param_group in optimizer.param_groups:

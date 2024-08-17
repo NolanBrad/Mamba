e83f249b28acde7789e067d9d879ec8c19abbd1d
Brad Nolan <brad@throttle.co.nz>
2024-08-18 10:52:18 +1200

    Bump to versio 0.1.12

--- src/MambaTSF/__init__.py
+++ src/MambaTSF/__init__.py
@@ -3,7 +3,7 @@
 # This is the version number of your code.  This will change each time you make
 # a new release.  The most popular approach is to use Semantic Versioning,
 # described at https://semver.org/
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 
 # Version History

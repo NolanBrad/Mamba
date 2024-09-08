9875e98f5b20178c89d7b1da3059342740fcee60
Brad Nolan <brad@throttle.co.nz>
2024-09-08 20:10:37 +1200

    First check in for Shift

---
+++ src/MambaTSF/Shift.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from typing import Optional
+
+
+@dataclass
+class ShiftConfig:
+    seqLen: int
+    shift: Optional[int] = None
+
+class Shift(nn.Module):
+    """
+    Use a Linear layer with a sigmoid activation to
+    determine how much to 'shift' or 'roll' the time sequence
+    """
+
+    def __init__(self, configs:ShiftConfig):
+
+        super(Shift, self).__init__()
+        self.configs = configs
+        self.shiftLinear = nn.Linear(configs.seqLen, 1, bias=True)
+
+    def forward(self, x):
+
+        _, L, _ = x.shape # B L N
+        # B: batch_size;
+        # L: seq_len;
+        # N: number of variate (tokens), can also includes covariates
+        assert L == self.configs.seqLen
+
+        # Shift determination
+        # B L N -> B N L
+        x_shift = x.permute(0, 2, 1)
+
+        # B L N -> B N 1
+        x_shift = self.shiftLinear(x_shift)
+        y_shift = F.sigmoid(x_shift) * self.configs.seqLen
+
+        y = torch.roll(x, y_shift, dims=2)
+
+        return y  # [B, L(shifted by y_shift), N]
